503c5f637b50bf03b9b2bda0a0d24acd5863474a
Leonardo Gamez <leonardo.gamez@h1ve.com.mx>
2024-04-09 19:19:59 -0600

    configure local ports catalog api

--- src/Services/Catalog/ECommerce.Catalog.API/Program.cs
+++ src/Services/Catalog/ECommerce.Catalog.API/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Add Dependencies  and services to di
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure Http Pipeline
 
 app.Run();

c1d80b05ef6fd10fbb8fb732ded8811f5f4f19be
Leonardo Gamez <leonardo.gamez@h1ve.com.mx>
2024-07-20 10:04:45 -0600

    create abstractions for ordering api DDD

---
+++ src/Services/Ordering/ECommerce.Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace ECommerce.Ordering.Domain.Abstractions;
+
+public interface IDomainEvent : INotification
+{
+    Guid Eventd=> Guid.NewGuid();
+    public DateTime OcurredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName;
+}

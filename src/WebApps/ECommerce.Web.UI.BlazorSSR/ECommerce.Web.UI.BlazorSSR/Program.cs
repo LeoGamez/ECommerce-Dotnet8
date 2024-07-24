212c6ad67071c016204c23045ca95c0b3f2534b0
Leonardo Gamez <leonardo.gamez@h1ve.com.mx>
2024-07-23 18:53:10 -0600

    created services and models for web ui

---
+++ src/WebApps/ECommerce.Web.UI.BlazorSSR/ECommerce.Web.UI.BlazorSSR/Program.cs
@@ -0,0 +1,52 @@
+using ECommerce.Web.UI.BlazorSSR.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var gatewayUrl = builder.Configuration["ApiSettings:GatewayAddress"];
+
+builder.Services.AddRefitClient<ICatalogService>()
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri(gatewayUrl!);
+    });
+builder.Services.AddRefitClient<IBasketService>()
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri(gatewayUrl!);
+    });
+builder.Services.AddRefitClient<IOrderingService>()
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri(gatewayUrl!);
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(ECommerce.Web.UI.BlazorSSR.Client._Imports).Assembly);
+
+app.Run();
---
+++ src/WebApps/ECommerce.Web.UI.BlazorSSR/ECommerce.Web.UI.BlazorSSR/Services/ICatalogService.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Web.UI.BlazorSSR.Models.Catalog;
+
+namespace ECommerce.Web.UI.BlazorSSR.Services;
+
+public interface ICatalogService
+{
+    [Get("/catalog-service/products?pageNumber={pageNumber}&pageSize=={pageSize}")]
+    Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
+    [Get("/catalog-service/products/{id}")]
+    Task<GetProductsResponse> GetProduct(Guid id);
+    [Get("/catalog-service/products/category/{category}")]
+    Task<GetProductsResponse> GetProductByCategory(string category);
+}
